2cedfc46ec3154c1b6c0b44ee4ee033c02b0a337
Tim-Tscheppe <timothy.tscheppe@marquette.edu>
2023-08-20 19:57:14 -0500

    Added support for printing and println

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-Writing a basic OS Kernel in Rus
+Writing a basic OS Kernel in Rust
 
 Using tutorial here as a starting point:
 https://os.phil-opp.com/
 
 To build:
 git clone https://github.com/Tim-Tscheppe/BlightKernel
-cargo build
+cargo run
 
 BlightKernel/target/x86_64-blight_kernel/debug/bootimage-blight_kernel.bin is where the boot image is located.
+The comfig files are located at BlightKernel/x86_64-blight_kernel.json and 
 You can copy it onto a USB drive or boot it on a VM
--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,22 @@
 // don't use normal entry point chain
 #![no_main]
 
+mod vga_buffer;
+
 // panic_handler defines the function the compiler should invoke when a panic occurs
 // since we disabled standard library, we need to define it ourselves
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
-
-static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    println!("Hello World{}", "!");
+    panic!("Panic Test");
     loop {}
 }
+
